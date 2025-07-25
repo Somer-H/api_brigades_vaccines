52558b0b9c1dd7f112f3f4652e76da553f9fb216
Somer-H <233424@ids.upchiapas.edu.mx>
2025-07-25 03:03:35 -0600

    ver brigadas

--- src/Vaccines/Presentation/Controllers/brigadeController.py
+++ src/Vaccines/Presentation/Controllers/brigadeController.py
@@ -1,5 +1,5 @@
-from ...Application.Services.brigadeService import createBrigadeService
-from ...Domain.Scheme.brigadeScheme import BrigateRequestScheme, BrigateResponseWithLocationsScheme
+from ...Application.Services.brigadeService import createBrigadeService, getBrigadesService
+from ...Domain.Scheme.brigadeScheme import BrigateRequestScheme, BrigateResponseWithLocationsScheme, BrigadeAndLocationsScheme
 from fastapi import Depends, HTTPException
 from ....Shared.mysql import get_db
 from ....Shared.auth import jwtAuth
@@ -15,3 +15,7 @@ def createBrigadeController(brigade: BrigateRequestScheme, db: Session = Depends
     if not brigade.locations: 
         raise HTTPException(status_code=400, detail="Debe ingresar al menos una localización")
     return createBrigadeService(brigade, db)
+
+def getBrigadesController(db: Session = Depends(get_db), userData = jwtAuth("director")) -> list[BrigadeAndLocationsScheme]: 
+    brigades = getBrigadesService(db)
+    return brigades
--- src/Vaccines/Presentation/Routes/brigadeRoutes.py
+++ src/Vaccines/Presentation/Routes/brigadeRoutes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from ..Controllers.brigadeController import createBrigadeController
+from ..Controllers.brigadeController import createBrigadeController, getBrigadesController
 
 router = APIRouter()
 
 router.post("/brigades") (createBrigadeController)
+router.get("/brigades") (getBrigadesController)
